8522dd594d6024b4bc2436bcf29d77f2238f0724
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-16 12:31:17 +0400

    Update web.php route file

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,31 @@ Route::get('/', function () {
 	return view('welcome');
 }) -> name('home');
 
+route::get(
+	'/exit', 
+	'App\Http\Controllers\UserController@logout'
+) -> name('logout');
+
+
+/* Authorization */
+
 Route::get('/auth', function () {
 	return view('auth');
 }) -> name('auth');
 
+Route::post(
+	'/auth',
+	'App\Http\Controllers\UserController@auth'
+) -> name('auth.sub');
+
+
+/* Registration */
+
 Route::get('/reg', function () {
 	return view('reg');
 }) -> name('reg');
+
+Route::post(
+	'/reg/sub',
+	'App\Http\Controllers\UserController@reg'
+) -> name('reg.sub');
